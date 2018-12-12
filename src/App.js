5615cd7b96bf7e7d9da49e8ea5a47ccdd2590388
Ana Mafla <anamafla@hotmail.com>
2018-12-12 09:24:18 -0500

    Add analytics

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Raffle from "./Raffle";
 import Home from "./Home";
 import About from "./About";
+import ReactGA from "react-ga";
+import NavBar from "./NavBar";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/raffle" component={Raffle} />
-          <Route path="/about" component={About} />
-        </Switch>
-      </div>
-    );
-  }
+export default function App() {
+  ReactGA.initialize("UA - 130806472 - 1");
+  ReactGA.pageview("/raffle");
+
+  return (
+    <div className="App">
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/raffle" component={Raffle} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </div>
+  );
 }
-
-export default App;
